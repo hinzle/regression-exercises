a8221d0404b6e3fc82556f7e2a462776e6888b2a
hinzle <bosque4d@hotmail.com>
2022-03-18 12:10:22 -0400

    2022 mar 18 : 12:09 EST: finished wrangle mvp

--- wrangle.py
+++ wrangle.py
@@ -21,8 +21,9 @@ def wrangle_zillow(use_cache=True):
     df=df[df.propertylandusedesc=='Single Family Residential']
     df=df.drop('propertylandusedesc',axis=1)
     df=df.drop_duplicates()
+    df=df.dropna()
     df=df.replace(r'^\s*$', np.nan, regex=True)
-    zillow=zillow.rename(
+    df=df.rename(
         {
         'yearbuilt':'year',
         'bedroomcnt':'beds',
@@ -31,46 +32,46 @@ def wrangle_zillow(use_cache=True):
         'taxvaluedollarcnt':'property_value',
         'taxamount':'taxes'
         },axis=1)
-    zillow.beds=zillow.beds.astype('string')
-    zillow.baths=zillow.baths.astype('string')
-    zillow.beds=zillow.beds.astype('string')
-    zillow.year=zillow.year.astype('Int64')
-    zillow.fips=zillow.fips.astype('Int64')
+    df.beds=df.beds.astype('string')
+    df.baths=df.baths.astype('string')
+    df.beds=df.beds.astype('string')
+    df.year=df.year.astype('Int64')
+    df.fips=df.fips.astype('Int64')
 
 
 
-	'''
-	takes in a dataframe and a target name, outputs three dataframes: 'train', 'validate', 'test', each stratified on the named target. 
+    # '''
+	# takes in a dataframe and a target name, outputs three dataframes: 'train', 'validate', 'test', each stratified on the named target. 
 	
-	->: str e.g. 'df.target_column'
-	<-: 3 x pandas.DataFrame ; 'train', 'validate', 'test'
+	# ->: str e.g. 'df.target_column'
+	# <-: 3 x pandas.DataFrame ; 'train', 'validate', 'test'
 
-	training set is 60% of total sample
-	validate set is 23% of total sample
-	test set is 17% of total sample
+	# training set is 60% of total sample
+	# validate set is 23% of total sample
+	# test set is 17% of total sample
 
-	'''
-	train, _ = train_test_split(df, train_size=.6, random_state=123, stratify=df[target_column])
-	validate, test = train_test_split(_, test_size=(3/7), random_state=123, stratify=_[target_column])
-	train.reset_index(drop=True, inplace=True)
-	validate.reset_index(drop=True, inplace=True)
-	test.reset_index(drop=True, inplace=True)
+	# '''
+	# train, _ = train_test_split(df, train_size=.6, random_state=123, stratify=df[target_column])
+	# validate, test = train_test_split(_, test_size=(3/7), random_state=123, stratify=_[target_column])
+	# train.reset_index(drop=True, inplace=True)
+	# validate.reset_index(drop=True, inplace=True)
+	# test.reset_index(drop=True, inplace=True)
 
-	'''
-	take in train, validate, and test DataFrames, impute mode for 'col',
-	and return train, validate, and test DataFrames
-	'''
-    col=[
-    'beds',
-    'baths',
-    'sqft',
-    'property_value',
-    'year',
-    'taxes',
-    ]
-	imputer = SimpleImputer(missing_values = np.NAN, strategy='most_frequent')
-	train[col] = imputer.fit_transform(train[col])
-	validate[col] = imputer.transform(validate[col])
-	test[col] = imputer.transform(test[col])
+	# '''
+	# take in train, validate, and test DataFrames, impute mode for 'col',
+	# and return train, validate, and test DataFrames
+	# '''
+    # col=[
+    # 'beds',
+    # 'baths',
+    # 'sqft',
+    # 'property_value',
+    # 'year',
+    # 'taxes',
+    # ]
+	# imputer = SimpleImputer(missing_values = np.NAN, strategy='most_frequent')
+	# train[col] = imputer.fit_transform(train[col])
+	# validate[col] = imputer.transform(validate[col])
+	# test[col] = imputer.transform(test[col])
 
-    return 0
+    return df
